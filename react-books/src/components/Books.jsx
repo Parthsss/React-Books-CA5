77bcf0cf8ec925f191c7f00f2f575abe57e50e17
Parthssh <naturallyinsane13@gmail.com>
2024-01-30 16:31:27 +0530

    Format V.1.5

--- react-books/src/components/Books.jsx
+++ react-books/src/components/Books.jsx
@@ -1,117 +1,12 @@
-// import React, { useEffect, useState } from "react";
-// import { NavLink } from "react-router-dom";
-// import axios from "axios";
-// import "./Books.css";
-
-
-// function BookList() {
-//   const [booksArray, setBooksArray] = useState([]);
-//   const [searchInput, setSearchInput] = useState("");
-
-//   useEffect(() => {
-//     async function getBooks() {
-//       try {
-//         const response = await axios.get(
-//           "https://reactnd-books-api.udacity.com/books",
-//           {
-//             headers: { Authorization: "whatever-you-want" },
-//           }
-//         );
-//         setBooksArray(response.data.books);
-//         console.log("Books: ", response.data.books);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-//     getBooks();
-//   }, []);
-
-//   const filteredBooks = booksArray.filter((book) => {
-//     if (searchInput === "") {
-//       return true;
-//     }
-//     const title = book.title.toLowerCase();
-//     return title.startsWith(searchInput.toLowerCase());
-//   });
-
-//   const handleSearch = (e) => {
-//     setSearchInput(e.target.value);
-//   };
-
-//   return (
-//     <div className="book-list-container">
-//       <div className="header">
-//         <img className="logo-pic" src="https://kalvium.community/images/sidebar-3d-logo.svg" alt="" />
-//         <h2>Kalvium Books</h2>
-
-
-// <div className="search-bar-container">
-
-//   <i className="fas fa-search search-icon"></i>
-//   <input
-//     className="search-input"
-//     type="text"
-//     placeholder="Search Books"
-//     onChange={handleSearch}
-//   />
-// </div>
-//         <NavLink to="/Forms">
-//           <button className="register-button">Register</button>
-//         </NavLink>
-//       </div>
-
-//       <div className="book-list">
-//         {searchInput === "" ? (
-//           booksArray.map((book) => (
-//             <div key={book.id} className="book-card">
-//               <div className="book-image">
-//                 <img
-//                   className="book-thumbnail"
-//                   src={book.imageLinks.thumbnail}
-//                   alt={book.title}
-//                 />
-//               </div>
-//               <div className="book-details">
-//                 <p className="book-title">{book.title}</p>
-//                 <p className="book-authors">
-//                   {book.authors.map((author, index) => (
-//                     <span key={index}>
-//                       {index > 0 && ', '}
-//                       {author}
-//                     </span>
-//                   ))}
-//                 </p>
-//                 <p className="book-rating">
-//                   Rating: ★ {book.averageRating || "--"}/5
-//                 </p>
-//                 <p>Free</p>
-//               </div>
-//             </div>
-//           ))
-//         ) : (
-//           <p>Please enter a valid name</p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default BookList;
-
-
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 import "./Books.css";
 
-
 function BookList() {
   const [booksArray, setBooksArray] = useState([]);
   const [searchInput, setSearchInput] = useState("");
 
-
-
-  
   useEffect(() => {
     async function getBooks() {
       try {
@@ -129,8 +24,7 @@ function BookList() {
     }
     getBooks();
   }, []);
-  
-  
+
   const filteredBooks = booksArray.filter((book) => {
     if (searchInput === "") {
       return true;
@@ -139,7 +33,6 @@ function BookList() {
     return title.startsWith(searchInput.toLowerCase());
   });
 
-  
   const handleSearch = (e) => {
     setSearchInput(e.target.value);
   };
@@ -147,11 +40,15 @@ function BookList() {
   return (
     <div className="book-list-container">
       <div className="header">
-        <img className="logo-pic" src="https://kalvium.community/images/sidebar-3d-logo.svg" alt="" />
+        <img
+          className="logo-pic"
+          src="https://kalvium.community/images/sidebar-3d-logo.svg"
+          alt=""
+        />
         <h2>Kalvium Books</h2>
         <div className="search-bar-container">
-        <i className="fas fa-search search-icon"></i>
-          
+          <i className="fas fa-search search-icon"></i>
+
           <input
             className="search-input"
             type="text"
@@ -159,7 +56,7 @@ function BookList() {
             onChange={handleSearch}
           />
         </div>
-        
+
         <NavLink to="/Forms">
           <button className="register-button">Register</button>
         </NavLink>
@@ -181,7 +78,7 @@ function BookList() {
                 <p className="book-authors">
                   {book.authors.map((author, index) => (
                     <span key={index}>
-                      {index > 0 && ', '}
+                      {index > 0 && ", "}
                       {author}
                     </span>
                   ))}
@@ -198,7 +95,6 @@ function BookList() {
           <p>Please enter valid name</p>
         )}
       </div>
-
     </div>
   );
 }
